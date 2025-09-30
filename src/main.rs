f3ffcd0d0d5f54ffa33c7d992e18af15ef9a2c0e
wivest <120594169+wivest@users.noreply.github.com>
2025-09-30 23:17:42 +0200

    feat: read file contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::OpenOptions;
+use std::io::{Error, Read};
+
+fn open_file(path: &str) -> Result<String, Error> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)?;
+    let mut content = String::from("");
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = ".gitignore";
+
+    match open_file(path) {
+        Ok(content) => println!("{}", content),
+        Err(e) => println!("Error {}!", e),
+    }
 }
