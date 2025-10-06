fefab1e4872a320daa00feb6b9384a38ceb798b8
wivest <120594169+wivest@users.noreply.github.com>
2025-10-06 15:44:33 +0200

    refactor: create separate functions

--- src/language.rs
+++ src/language.rs
@@ -7,38 +7,46 @@ pub struct Language {
 
 impl Language {
     pub fn new(pairs: Pairs<Rule>) -> Self {
-        let mut list = vec![];
-        for pair in pairs {
-            match pair.as_rule() {
-                Rule::program => {
-                    let instructions = pair.into_inner();
-                    for instruction in instructions {
-                        match instruction.as_rule() {
-                            Rule::instruction => {
-                                let ins = instruction.into_inner();
-                                for i in ins {
-                                    match i.as_rule() {
-                                        Rule::add => {
-                                            println!("add!");
-                                            list.push(Instruction::Add);
-                                        }
-                                        _ => {}
-                                    }
-                                }
-                            }
-                            _ => {
-                                println!("other...");
-                            }
-                        }
-                    }
-                }
-                _ => {}
-            }
-        }
+        let instructions = parse_program(pairs);
         Language {
             instructions: vec![],
         }
     }
+}
+
+fn parse_program(pairs: Pairs<Rule>) -> Option<Vec<Instruction>> {
+    let program = pairs.peek()?;
+    match program.as_rule() {
+        Rule::program => parse_instructions(program.into_inner()),
+        _ => None,
+    }
+}
+
+fn parse_instructions(pairs: Pairs<Rule>) -> Option<Vec<Instruction>> {
+    let mut list = vec![];
+
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::instruction => {
+                let instruction = pair.into_inner();
+                for rule in instruction {
+                    let i = match rule.as_rule() {
+                        Rule::add => {
+                            println!("add!");
+                            Some(Instruction::Add)
+                        }
+                        _ => None,
+                    };
+                    list.push(i?)
+                }
+            }
+            _ => {
+                println!("other ({:?})...", pair.as_rule());
+            }
+        }
+    }
+
+    Some(list)
 }
 
 enum Instruction {
