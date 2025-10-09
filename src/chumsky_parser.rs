c3a92aa979fdce36121eb9cefc1fe91909c6c11b
wivest <120594169+wivest@users.noreply.github.com>
2025-10-09 20:20:59 +0200

    feat: parse add instruction

--- src/chumsky_parser.rs
+++ src/chumsky_parser.rs
@@ -1,6 +1,21 @@
 use chumsky::prelude::*;
 
-fn register<'src>() -> impl Parser<'src, &'src str, &'src str> {
-    let register = just("x").ignore_then(text::int(10)).map(|idx| idx);
-    register
+enum Instruction {
+    Add { rd: i32, rs1: i32, rs2: i32 },
+}
+
+fn add<'src>() -> impl Parser<'src, &'src str, Instruction> {
+    let register = just("x")
+        .ignore_then(text::int(10))
+        .map(|s: &'src str| s.parse::<i32>().unwrap());
+
+    let add_parser = just("add")
+        .ignore_then(register.repeated().collect_exactly::<[_; 3]>())
+        .map(|[rd, rs1, rs2]| Instruction::Add {
+            rd: rd,
+            rs1: rs1,
+            rs2: rs2,
+        });
+
+    add_parser
 }
