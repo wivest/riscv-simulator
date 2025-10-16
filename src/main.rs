538e0fe29bbaf633a69dfc24df0b1480ce6e9928
wivest <120594169+wivest@users.noreply.github.com>
2025-10-16 14:05:20 +0200

    fix: remove write/create rights

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ mod chumsky_parser;
 mod processor;
 
 fn open_file(path: &str) -> Result<String, Error> {
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(path)?;
+    let mut file = OpenOptions::new().read(true).open(path)?;
     let mut content = String::from("");
     file.read_to_string(&mut content)?;
     Ok(content)
@@ -22,12 +18,8 @@ fn main() {
     if let Ok(content) = open_file(path) {
         let result = chumsky_parser::program().parse(&content).into_result();
         match result {
-            Ok(instructions) => {
-                processor::execute(instructions);
-            }
-            Err(err) => {
-                println!("{err:?}")
-            }
+            Ok(instructions) => processor::execute(instructions),
+            Err(err) => println!("{err:?}"),
         }
     } else {
         println!("File error!");
