cb6af07fb624a53dcb55dde66cdd8ba7a83e07ad
wivest <120594169+wivest@users.noreply.github.com>
2025-10-28 23:23:15 +0100

    feat: prototype U-type instruction

--- src/parser/instruction/mod.rs
+++ src/parser/instruction/mod.rs
@@ -4,12 +4,14 @@ pub use itype::IType;
 pub use jtype::JType;
 pub use rtype::RType;
 pub use stype::SType;
+pub use utype::UType;
 
 mod btype;
 mod itype;
 mod jtype;
 mod rtype;
 mod stype;
+mod utype;
 
 #[derive(Debug)]
 pub enum Instruction {
---
+++ src/parser/instruction/utype.rs
@@ -0,0 +1,16 @@
+use crate::processor::Processor;
+
+#[derive(Debug, Clone, Copy)]
+pub enum UType {
+    Li,
+}
+
+impl UType {
+    pub fn execute(&self, cpu: &mut Processor, rd: i32, imm: i32) {
+        match self {
+            UType::Li => {
+                todo!()
+            }
+        }
+    }
+}
