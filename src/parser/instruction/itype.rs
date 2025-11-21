cce5fd6e9f09e0c19d516fd83cf413d1a787d4e1
wivest <120594169+wivest@users.noreply.github.com>
2025-11-21 23:44:58 +0100

    feat: implement load half and word

--- src/parser/instruction/itype.rs
+++ src/parser/instruction/itype.rs
@@ -4,6 +4,8 @@ use crate::processor::Processor;
 pub enum IType {
     Addi,
     Lb,
+    Lh,
+    Lw,
 }
 
 impl IType {
@@ -14,6 +16,21 @@ impl IType {
                 let address = cpu.get_reg(rs);
                 let byte = *cpu.memory.get(&(address as usize)).unwrap_or(&0);
                 cpu.set_reg(rd, byte as i32);
+            }
+            IType::Lh => {
+                let address = cpu.get_reg(rs);
+                let low = *cpu.memory.get(&(address as usize)).unwrap_or(&0);
+                let high = *cpu.memory.get(&(address as usize + 1)).unwrap_or(&0);
+                cpu.set_reg(rd, (high as i32) << 8 + low as i32);
+            }
+            IType::Lw => {
+                let address = cpu.get_reg(rs);
+                let byte0 = *cpu.memory.get(&(address as usize)).unwrap_or(&0) as i32;
+                let byte1 = *cpu.memory.get(&(address as usize + 1)).unwrap_or(&0) as i32;
+                let byte2 = *cpu.memory.get(&(address as usize + 2)).unwrap_or(&0) as i32;
+                let byte3 = *cpu.memory.get(&(address as usize + 3)).unwrap_or(&0) as i32;
+                let word = byte3 << 24 + byte2 << 16 + byte1 << 8 + byte0;
+                cpu.set_reg(rd, word);
             }
         }
         cpu.pc += 4;
