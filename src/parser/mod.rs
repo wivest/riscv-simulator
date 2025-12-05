c109b07c11f0a2a8bf7b5ff287bd9ff6c2b20114
wivest <120594169+wivest@users.noreply.github.com>
2025-12-05 23:51:59 +0100

    refactor: move register and immediate to common

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,52 +1,13 @@
 use chumsky::prelude::*;
 
+use common::*;
 use real::instructions::Instruction;
 use real::instructions::{BType, IType, JType, RType, SType, UType};
 
+mod common;
 mod line;
 mod pseudo;
 pub mod real;
-
-fn number<'src, T: std::str::FromStr>() -> impl Parser<'src, &'src str, T> {
-    text::int(10).map(|s: &'src str| s.parse::<T>().ok().unwrap())
-}
-
-fn register<'src>() -> impl Parser<'src, &'src str, usize> {
-    let index = just("x").ignore_then(number()).filter(|n| *n <= 31);
-
-    let zero = just("zero").map(|_| 0);
-    let ra = just("ra").map(|_| 1);
-    let sp = just("sp").map(|_| 2);
-    let gp = just("gp").map(|_| 3);
-    let tp = just("tp").map(|_| 4);
-    let fp = just("fp").map(|_| 8);
-
-    let temporary = just("t")
-        .ignore_then(number::<usize>())
-        .filter(|n| *n <= 6)
-        .map(|n| if n <= 2 { n + 5 } else { n + 25 });
-
-    let saved = just("s")
-        .ignore_then(number::<usize>())
-        .filter(|n| *n <= 11)
-        .map(|n| if n <= 1 { n + 8 } else { n + 16 });
-
-    let argument = just("a")
-        .ignore_then(number::<usize>())
-        .filter(|n| *n <= 7)
-        .map(|n| n + 10);
-
-    choice((index, zero, ra, sp, gp, tp, fp, temporary, saved, argument)).padded()
-}
-
-fn immediate<'src>(bits: u32) -> impl Parser<'src, &'src str, i32> {
-    let sign = just("-").map(|_| -1).or(empty().map(|_| 1));
-    sign.then_ignore(text::whitespace())
-        .then(number::<i32>())
-        .filter(move |(_, n)| 0u32.leading_zeros() - n.leading_zeros() <= bits)
-        .map(|(s, n)| s * n)
-        .padded()
-}
 
 fn rtype<'src>(
     name: RType,
@@ -221,63 +182,6 @@ pub fn pre_parse<'src>() -> impl Parser<'src, &'src str, ()> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_register_x() {
-        let result = register().parse("x10");
-        assert_eq!(result.unwrap(), 10);
-    }
-
-    #[test]
-    fn test_register_name() {
-        let zero = register().parse("zero");
-        let ra = register().parse("ra");
-        let sp = register().parse("sp");
-        let gp = register().parse("gp");
-        let tp = register().parse("tp");
-        let fp = register().parse("fp");
-
-        assert_eq!(zero.unwrap(), 0);
-        assert_eq!(ra.unwrap(), 1);
-        assert_eq!(sp.unwrap(), 2);
-        assert_eq!(gp.unwrap(), 3);
-        assert_eq!(tp.unwrap(), 4);
-        assert_eq!(fp.unwrap(), 8);
-    }
-
-    #[test]
-    fn test_register_name_index() {
-        let t6 = register().parse("t6");
-        let a7 = register().parse("a7");
-        let s11 = register().parse("s11");
-
-        assert_eq!(t6.unwrap(), 31);
-        assert_eq!(a7.unwrap(), 17);
-        assert_eq!(s11.unwrap(), 27);
-
-        let t7 = register().parse("t7");
-        let a8 = register().parse("a8");
-        let s12 = register().parse("s12");
-
-        assert_eq!(t7.has_errors(), true);
-        assert_eq!(a8.has_errors(), true);
-        assert_eq!(s12.has_errors(), true);
-    }
-
-    #[test]
-    fn test_immediate() {
-        let result = immediate(32).parse("42");
-        assert_eq!(result.unwrap(), 42);
-        let result = immediate(32).parse("-42");
-        assert_eq!(result.unwrap(), -42);
-        let result = immediate(32).parse("- 42");
-        assert_eq!(result.unwrap(), -42);
-
-        let result = immediate(12).parse("4095");
-        assert_eq!(result.unwrap(), 4095);
-        let result = immediate(12).parse("4096");
-        assert_eq!(result.has_errors(), true);
-    }
 
     #[test]
     fn test_rtype() {
