513dceccd53fff8bf3fabb3b587056ab3b3d4096
wivest <120594169+wivest@users.noreply.github.com>
2025-12-06 23:19:44 +0100

    feat: expand to Vec

--- src/parser/pseudo/instruction.rs
+++ src/parser/pseudo/instruction.rs
@@ -1,18 +1,26 @@
+use crate::parser::real::instructions::{IType, Instruction, UType};
+
 #[derive(Debug)]
 pub enum Pseudo {
     Li { rd: usize, imm: i32 },
 }
 
 impl Pseudo {
-    pub fn expand(self) -> String {
+    pub fn expand(self) -> Vec<Instruction> {
         match self {
-            Self::Li { rd, imm } => format!(
-                "lui x{}, {}\naddi x{}, x0, {}",
-                rd,
-                imm >> 12,
-                rd,
-                imm << 20 >> 20
-            ),
+            Self::Li { rd, imm } => vec![
+                Instruction::UType {
+                    name: UType::Lui,
+                    rd,
+                    imm: imm >> 12,
+                },
+                Instruction::IType {
+                    name: IType::Addi,
+                    rd,
+                    rs: 0,
+                    imm: imm << 20 >> 20,
+                },
+            ],
         }
     }
 }
