4e2eb44a281736c96551c96b3d098b883f5ab2c6
wivest <120594169+wivest@users.noreply.github.com>
2025-12-09 23:26:52 +0100

    fix: produce only real instructions

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -9,13 +9,6 @@ mod pseudo;
 pub mod real;
 
 pub fn program<'src>() -> impl Parser<'src, &'src str, Vec<Instruction>> {
-    let real_ins = real::real_instructions();
-    let pseudo_ins = pseudo::pseudo_instructions();
-
-    real_ins.padded().repeated().collect()
-}
-
-pub fn pre_parser<'src>() -> impl Parser<'src, &'src str, Vec<Instruction>> {
     let real_ins = real::real_instructions().map(|r| Line::Instruction(r));
     let pseudo_ins = pseudo::pseudo_instructions().map(|p| Line::Pseudo(p));
     let lines = choice((real_ins, pseudo_ins));
