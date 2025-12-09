6d8391af554b83ee950cb00adc8c70d43336934e
wivest <120594169+wivest@users.noreply.github.com>
2025-12-09 23:24:29 +0100

    feat: flatten pseudo

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -15,10 +15,18 @@ pub fn program<'src>() -> impl Parser<'src, &'src str, Vec<Instruction>> {
     real_ins.padded().repeated().collect()
 }
 
-pub fn pre_parser<'src>() -> impl Parser<'src, &'src str, Vec<Line>> {
+pub fn pre_parser<'src>() -> impl Parser<'src, &'src str, Vec<Instruction>> {
     let real_ins = real::real_instructions().map(|r| Line::Instruction(r));
     let pseudo_ins = pseudo::pseudo_instructions().map(|p| Line::Pseudo(p));
     let lines = choice((real_ins, pseudo_ins));
 
-    lines.padded().repeated().collect()
+    empty()
+        .map(|_| vec![])
+        .foldl(lines.padded().repeated(), |mut acc, l| {
+            acc.extend(match l {
+                Line::Instruction(r) => vec![r],
+                Line::Pseudo(p) => p.expand(),
+            });
+            acc
+        })
 }
