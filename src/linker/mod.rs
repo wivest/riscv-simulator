c7802f1de6ce7ba471710b13e6af44ae90dd5b88
wivest <120594169+wivest@users.noreply.github.com>
2026-01-31 01:55:42 +0100

    feat: map parser to processor

--- src/linker/mod.rs
+++ src/linker/mod.rs
@@ -4,23 +4,33 @@ use crate::processor::instructions::Instruction as ProcInstr;
 pub fn translate(parsed: Vec<ParsInstr>) -> Vec<ProcInstr> {
     parsed
         .iter()
-        .map(|i| match i {
+        .map(|i| match *i {
             ParsInstr::BType {
                 name,
                 rs1,
                 rs2,
                 offset,
-            } => todo!(),
-            ParsInstr::IType { name, rd, rs, imm } => todo!(),
-            ParsInstr::JType { name, rd, imm } => todo!(),
-            ParsInstr::RType { name, rd, rs1, rs2 } => todo!(),
+            } => ProcInstr::BType {
+                name,
+                rs1,
+                rs2,
+                offset,
+            },
+            ParsInstr::IType { name, rd, rs, imm } => ProcInstr::IType { name, rd, rs, imm },
+            ParsInstr::JType { name, rd, imm } => ProcInstr::JType { name, rd, imm },
+            ParsInstr::RType { name, rd, rs1, rs2 } => ProcInstr::RType { name, rd, rs1, rs2 },
             ParsInstr::SType {
                 name,
                 rs1,
                 rs2,
                 imm,
-            } => todo!(),
-            ParsInstr::UType { name, rd, imm } => todo!(),
+            } => ProcInstr::SType {
+                name,
+                rs1,
+                rs2,
+                imm,
+            },
+            ParsInstr::UType { name, rd, imm } => ProcInstr::UType { name, rd, imm },
         })
         .collect()
 }
