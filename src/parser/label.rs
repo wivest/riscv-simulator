b4348fe779d9552bc17a718cb58f34a1e6c79fd2
wivest <120594169+wivest@users.noreply.github.com>
2026-03-23 10:07:48 +0100

    test: parse labels

--- src/parser/label.rs
+++ src/parser/label.rs
@@ -1,3 +1,4 @@
+use crate::parser::common::HPadded;
 use chumsky::prelude::*;
 
 #[derive(Debug, PartialEq)]
@@ -6,11 +7,48 @@ pub struct Reference(String);
 pub struct Definition(String);
 
 pub fn label_ref<'src>() -> impl Parser<'src, &'src str, Reference> {
-    text::ascii::ident().map(|label: &str| Reference(label.to_owned()))
+    text::ascii::ident()
+        .h_padded()
+        .map(|label: &str| Reference(label.to_owned()))
 }
 
 pub fn label_def<'src>() -> impl Parser<'src, &'src str, Definition> {
     text::ascii::ident()
+        .h_padded()
         .then_ignore(just(":"))
+        .h_padded()
         .map(|label: &str| Definition(label.to_owned()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn label_reference() {
+        let result = label_ref().parse("label");
+        assert_eq!(result.unwrap(), Reference("label".to_owned()));
+        let result = label_ref().parse("  label // comment");
+        assert_eq!(result.unwrap(), Reference("label".to_owned()));
+        let result = label_ref().parse("__4lphanuM");
+        assert_eq!(result.unwrap(), Reference("__4lphanuM".to_owned()));
+        let result = label_ref().parse("42");
+        assert_eq!(result.has_errors(), true);
+        let result = label_ref().parse("label:");
+        assert_eq!(result.has_errors(), true);
+    }
+
+    #[test]
+    fn label_definition() {
+        let result = label_def().parse("label:");
+        assert_eq!(result.unwrap(), Definition("label".to_owned()));
+        let result = label_def().parse("  label : // comment");
+        assert_eq!(result.unwrap(), Definition("label".to_owned()));
+        let result = label_def().parse("__4lphanuM:");
+        assert_eq!(result.unwrap(), Definition("__4lphanuM".to_owned()));
+        let result = label_def().parse("42:");
+        assert_eq!(result.has_errors(), true);
+        let result = label_def().parse("label");
+        assert_eq!(result.has_errors(), true);
+    }
 }
